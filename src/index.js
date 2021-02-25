902a76665d904d6ae9a6d8499256ebf5c7399ccc
Sergey Demikhov <demikhov.sergey@gmail.com>
2021-02-25 21:02:54 +0400

    remove unnecessary getter

--- src/index.js
+++ src/index.js
@@ -11,17 +11,13 @@ const TYPES_BY_EXTENSION = {
   '.yaml': 'yaml',
 };
 
-const getTypeFromFilePath = (filePath) => {
-  const extension = path.extname(filePath);
-  return TYPES_BY_EXTENSION[extension];
-};
-
 const loadFile = (filePath) => {
   const absoluteFilePath = path.resolve(process.cwd(), filePath);
   const fileContent = fs.readFileSync(absoluteFilePath, 'utf-8');
 
-  const type = getTypeFromFilePath(filePath);
-  const obj = parsers[type](fileContent);
+  const extension = path.extname(filePath);
+  const parserType = TYPES_BY_EXTENSION[extension];
+  const obj = parsers[parserType](fileContent);
   return obj;
 };
 
